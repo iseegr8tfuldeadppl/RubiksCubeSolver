1443c5ccb13017cc48eae25247271132eda86ea3
K R E A M <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2021-09-22 15:58:14 +0100

    arduino code improved

--- RubiksCubeMachine/new_version/simple_servo_angle_deciding_using_potentiometers/simple_servo_angle_deciding_using_potentiometers.ino
+++ RubiksCubeMachine/new_version/simple_servo_angle_deciding_using_potentiometers/simple_servo_angle_deciding_using_potentiometers.ino
@@ -81,329 +81,224 @@ int right_cw_start = 9, right_cw_end = 129, right_acw_start = 108, right_acw_end
 int right_sleeve_back = 180, right_sleeve_forth = 0;
 int right_sleeve_val = right_sleeve_back, right_wrist_val = right_cw_start;
 
-char string[] = "RLDlUBRDrBu";
-String messago = "";
 int moves_index = 0;
-String runMode="serial"; //Modes available: stringiteration          clockwiseanticlockwiselooptest
-    
+String moves_to_make = "";
+bool move = false;
+int tempdelay = 50;
+
 void setup() {
 
   // Step 1: Serial
   Serial.begin(9600);
 
   // Step 2: Attach servos
-  left_sleeve.attach(left_sleeve_pin);
-  left_wrist.attach(left_wrist_pin);
-  up_sleeve.attach(up_sleeve_pin);
-  up_wrist.attach(up_wrist_pin);
-  down_sleeve.attach(down_sleeve_pin);
-  down_wrist.attach(down_wrist_pin);
-  back_sleeve.attach(back_sleeve_pin);
-  back_wrist.attach(back_wrist_pin);
-  right_sleeve.attach(right_sleeve_pin);
-  right_wrist.attach(right_wrist_pin);
-
+  attach_motors();
   resetMotorPositions();
-
-  Serial.print("runMode=" + runMode);
 }
 
 void loop() {    
+  receiveCommand();
   if(debugging){
-    readSerial();
-    
-    // Step 1: read potentiometer values
-    next_poten1 = analogRead(potentiometer1); next_poten2 = analogRead(potentiometer2); next_poten3 = analogRead(potentiometer3);
-  
-    // Step 2: check if there was an update from potentiometers
-    if(previous_poten1!=next_poten1 ||  previous_poten2!=next_poten2 || previous_poten3!=next_poten3){
-      
-      // Step 3: if so update our potentiometer values
-      previous_poten1 = next_poten1; previous_poten2 = next_poten2; previous_poten3 = next_poten3;
-  
-      // Step 4: remap them and send them to our servos
-      switch(mode){
-        case 0:
-        applyPotentiometerToFace(left_sleeve, left_wrist, left_sleeve_val, left_wrist_val);
-          break;
-        case 1:
-          applyPotentiometerToFace(right_sleeve, right_wrist, right_sleeve_val, right_wrist_val);
-          break;
-        case 2:
-          applyPotentiometerToFace(up_sleeve, up_wrist, up_sleeve_val, up_wrist_val);
-          break;
-        case 3:
-          applyPotentiometerToFace(down_sleeve, down_wrist, down_sleeve_val, down_wrist_val);
-          break;
-        case 4:
-          applyPotentiometerToFace(back_sleeve, back_wrist, back_sleeve_val, back_wrist_val);
-          break;
-      }
-
-    }
+    controlUsingPotentiometers();
   } else {
-      if(runMode=="serial"){
-        if(messago=="")
-          readMovesFromSerial();
-        else {
-          char c = messago[moves_index];
-          switch(c){
-            case 'L':
-              L(true);
-              break;
-            case 'l':
-              L(false);
-              break;
-            case 'R':
-              R(true);
-              break;
-            case 'r':
-              R(false);
-              break;
-            case 'U':
-              U(true);
-              break;
-            case 'u':
-              U(false);
-              break;
-            case 'D':
-              D(true);
-              break;
-            case 'd':
-              D(false);
-              break;
-            case 'B':
-              B(true);
-              break;
-            case 'b':
-              B(false);
-              break;
-            default:
-              Serial.println("Unknown move in string: " + String(c));
-              Serial.println("Fail");
-              break;
-          }
-          
-          // increment index
-          moves_index += 1;
-          // if we reachd the end of the string loop back to the starat
-          if(moves_index>=strlen(string)){
-            messago = "";
-            moves_index = 0;
-            Serial.println("Ok");
-          }
-        }
-    }     
-    if(enabled){
-      if(runMode=="stringiteration"){
-        readSerial();
-
-        // reset all motors before starting sequence
-        if(moves_index==0)
-          resetMotorPositions();
-        
-        char c = string[moves_index];
-        switch(c){
-          case 'L':
-            L(true);
-            break;
-          case 'l':
-            L(false);
-            break;
-          case 'R':
-            R(true);
-            break;
-          case 'r':
-            R(false);
-            break;
-          case 'U':
-            U(true);
-            break;
-          case 'u':
-            U(false);
-            break;
-          case 'D':
-            D(true);
-            break;
-          case 'd':
-            D(false);
-            break;
-          case 'B':
-            B(true);
-            break;
-          case 'b':
-            B(false);
-            break;
-          default:
-            Serial.println("Unknown move in string: " + String(c));
-            break;
-        }
-        
-        // increment index
-        moves_index += 1;
-        // if we reachd the end of the string loop back to the starat
-        if(moves_index>=strlen(string))
-          moves_index = 0;
-      } else if(runMode=="clockwiseanticlockwiselooptest"){
-        readSerial();
-        // predefined set of moves
-        // these values are just clockwise/anti-clockwise loops to test each face
-        switch(mode){
-          case 0: // LEFT
-            L(true); // clockwise
-            delay(1500);
-            L(false); // anti-clockwise
-            delay(1500);
-            break;
-          case 1: // RIGHT
-            R(true); // clockwise
-            delay(1500);
-            R(false); // anti-clockwise
-            delay(1500);
-            break;
-          case 2: // UP
-            U(true); // clockwise
-            delay(1500);
-            U(false); // anti-clockwise
-            delay(1500);
-            break;
-          case 3: // DOWN
-            D(true); // clockwise
-            delay(1500);
-            D(false); // anti-clockwise
-            delay(1500);
-            break;
-          case 4: // BACK
-            B(true); // clockwise
-            delay(1500);
-            B(false); // anti-clockwise
-            delay(1500);
-            break;
-        } 
-      }
-
-    }
-
+    if(move)
+      performReceivedMoves();
   }
-
 }
 
+/*
+SELECT <L,R,U,D,B>
+DEBUG <1,0>
+MOTORS <RESET,ON,OFF>
+MOVE <LlRrUuDdBb> # MOVE is followed by a string of letters signifying the faces, upper case signifies clockwise turn, lower case signifies anti-clockwise
+ANGLE <WVER,WHOR,SVER,SHOR> <VALUE>
+ANGLE <W,S> <VALUE>
+*/
 
-void readMovesFromSerial(){
+
+void receiveCommand(){
+
+  // Don't read from serial until you perform all of the moves you've received
+  if(move)
+    return;
+
   if(Serial.available()>0){
-    String message = Serial.readStringUntil('\n');
-    if(message=='1'){
-        Serial.println("DEBUG MODE: Exited debug mode");
-        resetMotorPositions();
-        debugging = false;
-    } else if(message=='0'){
-        Serial.println("DEBUG MODE: Entered debug mode");
-        resetMotorPositions();
-        debugging = true;
-    } else if(message=='e'){
-        Serial.println("Motors enabled");
-        resetMotorPositions();
-        enabled = true;
-    } else if(message=='s'){
-        Serial.println("Motors disabled");
-        resetMotorPositions();
-        enabled = false;
-    } else if(message=='p'){
-        Serial.println("runMode=stringiteration");
-        runMode="stringiteration";
-    } else if(message=='o'){
-        Serial.println("runMode=clockwiseanticlockwiselooptest");
-        runMode="clockwiseanticlockwiselooptest";
-    } else {
-      if(messago!=""){
-        Serial.println("Fail");
-      } else {
-        messago = message;
+    String request = Serial.readStringUntil('\n');
+    String command = getValue(request, ' ', 0);
+
+    // only allow selecting faces if we're in debug
+    if(debugging){
+      if(command=="SELECT"){
+        selectFace(command);
+        Serial.println("Ok");
+        return;
       }
+    }
+
+    if(command=="DEBUG"){
+      String state = getValue(request, ' ', 1);
+      debug = state=="1";
+      Serial.println("Ok");
+      return;
+    } else if(command=="MOTORS"){
+      String order = getValue(request, ' ', 1);
+      if(order=="RESET"){
+        resetMotorPositions();
+        Serial.println("Ok");
+        return;
+      } else if(order=="ON"){
+        enabled = true;
+        Serial.println("Ok");
+        return;
+      } else if(order=="OFF"){
+        enabled = false;
+        Serial.println("Ok");
+        return;
+      }
+    } else if(command=="MOVE"){
+      moves_to_make = getValue(request, ' ', 1);
+      moves_index = 0;
+      move = true;
+      return;
+    } else if(command=="ANGLE"){
+      String face = getValue(request, ' ', 1);
+      if(face!=""){
+        String rest_of_command = getValue(request, ' ', 2);
+        setAngle(face, rest_of_command);
+        Serial.println("Ok");
+        return;
+      }
+    }
+  }
+  Serial.println("No");
+}
+
+void preprocessManualAngleSet(String value, Servo &sleeve, Servo &wrist, int &min_s, int &max_s, int &min_w, int &max_w, int &typical_s, int &typical_w){
+  if(value=="WVER"){
+    applyPotentiometerToFace(sleeve, wrist, typical_s, min_w);
+  } else if(value=="WHOR"){
+    applyPotentiometerToFace(sleeve, wrist, typical_s, max_w);
+  } if(value=="SVER"){
+    applyPotentiometerToFace(sleeve, wrist, min_s, typical_w);
+  } else if(value=="SHOR"){
+    applyPotentiometerToFace(sleeve, wrist, max_s, typical_w);
+  } else {
+    int val = getValue(value, ' ', 1).toInt();
+    if(getValue(value, ' ', 0)=="W"){
+      applyPotentiometerToFace(sleeve, wrist, typical_s, val);
+    } else {
+      applyPotentiometerToFace(sleeve, wrist, val, typical_w);
     }
   }
 }
 
-void readSerial(){
-  while(Serial.available()>0){
-    char letter = Serial.read();
-    switch(letter){
+void setAngle(String face, String value){
+    // Step 4: remap them and send them to our servos
+    switch(face[0]){
       case 'L':
-        Serial.println("DEBUG MODE: Selected face is L");
-        mode = 0;
+        preprocessManualAngleSet(value, left_sleeve, left_wrist, left_cw_start, left_acw_start, left_sleeve_back, left_sleeve_forth, left_sleeve_val, left_wrist_val);
         break;
       case 'R':
-        Serial.println("DEBUG MODE: Selected face is R");
-        mode = 1;
+        preprocessManualAngleSet(value, right_sleeve, right_wrist, right_cw_start, right_acw_start, right_sleeve_back, right_sleeve_forth, right_sleeve_val, right_wrist_val);
         break;
       case 'U':
-        Serial.println("DEBUG MODE: Selected face is U");
-        mode = 2;
+        preprocessManualAngleSet(value, up_sleeve, up_wrist, up_cw_start, up_acw_start, up_sleeve_back, up_sleeve_forth, up_sleeve_val, up_wrist_val);
         break;
       case 'D':
-        Serial.println("DEBUG MODE: Selected face is D");
-        mode = 3;
+        preprocessManualAngleSet(value, down_sleeve, down_wrist, down_cw_start, down_acw_start, down_sleeve_back, down_sleeve_forth, down_sleeve_val, down_wrist_val);
         break;
       case 'B':
-        Serial.println("DEBUG MODE: Selected face is B");
-        mode = 4;
-        break;
-      case '1':
-        Serial.println("DEBUG MODE: Exited debug mode");
-        resetMotorPositions();
-        debugging = false;
-        break;
-      case '0':
-        Serial.println("DEBUG MODE: Entered debug mode");
-        resetMotorPositions();
-        debugging = true;
-        break;
-      case 'e':
-        Serial.println("Motors enabled");
-        resetMotorPositions();
-        enabled = true;
-        break;
-      case 's':
-        Serial.println("Motors disabled");
-        resetMotorPositions();
-        enabled = false;
-        break;
-      case 'm':
-        resetMotorPositions();
-        break;
-      case 'p':
-        Serial.println("runMode=stringiteration");
-        runMode="stringiteration";
-        moves_index = 0;
-        break;
-      case 'o':
-        Serial.println("runMode=clockwiseanticlockwiselooptest");
-        runMode="clockwiseanticlockwiselooptest";
-        moves_index = 0;
-        break;
-      case 'g':
-        Serial.println("runMode=serial");
-        runMode="serial";
-        moves_index = 0;
-        break;
-      case '\n':
-        // just a new line indicator, comes at the end of every message
-        break;
-      default:
-        Serial.println("Error: letter " + String(letter) + " is not recognized as a command");
-        Serial.println("Type either L, R, U, D or B to choose a face");
-        if(debugging){
-          Serial.println("DEBUG MODE: After that you can control it with the two knobs");
-        } else {
-          Serial.println("Type e to enable motors or d to disable motors");
-        }
+        preprocessManualAngleSet(value, back_sleeve, back_wrist, back_cw_start, back_acw_start, back_sleeve_back, back_sleeve_forth, back_sleeve_val, back_wrist_val);
         break;
     }
+}
+
+void controlUsingPotentiometers(){
+  // Step 1: read potentiometer values
+  next_poten1 = analogRead(potentiometer1); next_poten2 = analogRead(potentiometer2); next_poten3 = analogRead(potentiometer3);
+
+  // Step 2: check if there was an update from potentiometers
+  if(previous_poten1!=next_poten1 ||  previous_poten2!=next_poten2 || previous_poten3!=next_poten3){
+    
+    // Step 3: if so update our potentiometer values
+    previous_poten1 = next_poten1; previous_poten2 = next_poten2; previous_poten3 = next_poten3;
+
+    // Step 4: remap them and send them to our servos
+    switch(mode){
+      case 0:
+      applyPotentiometerToFace(left_sleeve, left_wrist, left_sleeve_val, left_wrist_val);
+        break;
+      case 1:
+        applyPotentiometerToFace(right_sleeve, right_wrist, right_sleeve_val, right_wrist_val);
+        break;
+      case 2:
+        applyPotentiometerToFace(up_sleeve, up_wrist, up_sleeve_val, up_wrist_val);
+        break;
+      case 3:
+        applyPotentiometerToFace(down_sleeve, down_wrist, down_sleeve_val, down_wrist_val);
+        break;
+      case 4:
+        applyPotentiometerToFace(back_sleeve, back_wrist, back_sleeve_val, back_wrist_val);
+        break;
+    }
+
+  }
+}
+
+void performReceivedMoves(){
+
+  if(moves_to_make=="")
+    return;
+    
+  char c = moves_to_make[moves_index];
+  switch(c){
+    case 'L':
+      L(true);
+      break;
+    case 'l':
+      L(false);
+      break;
+    case 'R':
+      R(true);
+      break;
+    case 'r':
+      R(false);
+      break;
+    case 'U':
+      U(true);
+      break;
+    case 'u':
+      U(false);
+      break;
+    case 'D':
+      D(true);
+      break;
+    case 'd':
+      D(false);
+      break;
+    case 'B':
+      B(true);
+      break;
+    case 'b':
+      B(false);
+      break;
+    default:
+      Serial.println("No");
+      moves_to_make = "";
+      break;
+  }
+  
+  // increment index
+  moves_index += 1;
+  // if we reachd the end of the string loop back to the starat
+  if(moves_index>=strlen(moves_to_make) || moves_to_make==""){
+    moves_to_make = "";
+    moves_index = 0;
+    Serial.println("Ok");
   }
 }
 
 void resetMotorPositions(){
-  Serial.println("Resetted all motor positions");
   // Step 3: Retract servos
   // LEFT
   left_sleeve.write(left_sleeve_val);
@@ -426,18 +321,6 @@ void resetMotorPositions(){
   right_wrist.write(right_wrist_val);
 }
 
-void applyPotentiometerToFace(Servo &sleeve, Servo &wrist, int &sleeve_val, int &wrist_val){
-    sleeve_val = map(previous_poten1, 0, 1023, 0, 180);
-    wrist_val = map(previous_poten2, 0, 1023, 0, 180);
-    sleeve.write(sleeve_val);
-    wrist.write(wrist_val);
-
-    // Servo 5: display potentiometer values
-    Serial.println("Sleeve: " + String(sleeve_val, DEC) + 
-                    " Wrist: " + String(wrist_val, DEC));
-}
-
-int tempdelay = 50;
 void R(bool clockwise){
     if(clockwise){
       turn(right_wrist, right_sleeve, right_wrist_val, right_sleeve_val, right_cw_start, right_cw_end, right_sleeve_back, right_sleeve_forth, MEDIUM_SPEED);
@@ -445,7 +328,6 @@ void R(bool clockwise){
       turn(right_wrist, right_sleeve, right_wrist_val, right_sleeve_val, right_acw_start, right_acw_end, right_sleeve_back, right_sleeve_forth, MEDIUM_SPEED);
     }
 }
-
 void B(bool clockwise){
   
     // bring up wrist in to pin middle layer in place
@@ -462,7 +344,6 @@ void B(bool clockwise){
     // pull up wrist back up
     go(up_sleeve, up_sleeve_val, up_sleeve_back, MEDIUM_SPEED);
 }
-
 void L(bool clockwise){
     if(clockwise){
       turn(left_wrist, left_sleeve, left_wrist_val, left_sleeve_val, left_cw_start, left_cw_end, left_sleeve_back, left_sleeve_forth, MEDIUM_SPEED);
@@ -470,7 +351,6 @@ void L(bool clockwise){
       turn(left_wrist, left_sleeve, left_wrist_val, left_sleeve_val, left_acw_start, left_acw_end, left_sleeve_back, left_sleeve_forth, MEDIUM_SPEED);
     }
 }
-
 void D(bool clockwise){
     if(clockwise){
       turn(down_wrist, down_sleeve, down_wrist_val, down_sleeve_val, down_cw_start, down_cw_end, down_sleeve_back, down_sleeve_forth, MEDIUM_SPEED);
@@ -486,6 +366,61 @@ void U(bool clockwise){
     }
 }
 
+void selectFace(String command){
+  String face = getValue(command, ' ', 1);
+  if(face==""){
+    Serial.println("No");
+    return;
+  }
+
+  switch(face[0]){
+    case 'L':
+      mode = 0;
+      break;
+    case 'R':
+      mode = 1;
+      break;
+    case 'U':
+      mode = 2;
+      break;
+    case 'D':
+      mode = 3;
+      break;
+    case 'B':
+      mode = 4;
+      break;
+    default:
+      Serial.println("No");
+      return;
+      break;
+  }
+  Serial.println("Ok");
+}
+
+void applyPotentiometerToFace(Servo &sleeve, Servo &wrist, int &sleeve_val, int &wrist_val){
+    sleeve_val = map(previous_poten1, 0, 1023, 0, 180);
+    wrist_val = map(previous_poten2, 0, 1023, 0, 180);
+    sleeve.write(sleeve_val);
+    wrist.write(wrist_val);
+
+    // Servo 5: display potentiometer values
+    Serial.println("Sleeve: " + String(sleeve_val, DEC) + 
+                    " Wrist: " + String(wrist_val, DEC));
+}
+
+void attach_motors(){
+  left_sleeve.attach(left_sleeve_pin);
+  left_wrist.attach(left_wrist_pin);
+  up_sleeve.attach(up_sleeve_pin);
+  up_wrist.attach(up_wrist_pin);
+  down_sleeve.attach(down_sleeve_pin);
+  down_wrist.attach(down_wrist_pin);
+  back_sleeve.attach(back_sleeve_pin);
+  back_wrist.attach(back_wrist_pin);
+  right_sleeve.attach(right_sleeve_pin);
+  right_wrist.attach(right_wrist_pin);
+}
+
 void turn(Servo &wrist, Servo &sleeve, int &wrist_val, int &sleeve_val, int Start, int End, int back, int forth, int Speed){
   go(wrist, wrist_val, Start, Speed);
   go(sleeve, sleeve_val, forth, Speed);
@@ -496,6 +431,12 @@ void turn(Servo &wrist, Servo &sleeve, int &wrist_val, int &sleeve_val, int Star
 }
 
 void go(Servo &servo, int &departing_angle, int destination_angle, int Speed){
+
+  if(destination_angle>180)
+    destination_angle = 180;
+  else if(destination_angle<0)
+    destination_angle = 0;
+
   int Direction = destination_angle > departing_angle? 1 : -1;
   int difference = abs(destination_angle - departing_angle);
   for(int i=0; i<difference; i++){
@@ -505,4 +446,19 @@ void go(Servo &servo, int &departing_angle, int destination_angle, int Speed){
   }
 
   departing_angle = destination_angle;
+}
+
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+      if (data.charAt(i) == separator || i == maxIndex) {
+          found++;
+          strIndex[0] = strIndex[1] + 1;
+          strIndex[1] = (i == maxIndex) ? i+1 : i;
+      }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
