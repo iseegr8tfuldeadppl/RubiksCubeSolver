1ded2928934324589b699bd20c8cef8d5de762de
K R E A M <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2022-06-14 16:13:34 +0100

    working five face solver alg

---
+++ NemaMachine/Code/serverBatFiles/ClientofServer.bat
@@ -0,0 +1,8 @@
+title Cube Algorithm Solution Server
+
+
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+d:
+cd D:\Main\RubiksCubeSolver\NemaMachine\Code\brain\RubiksCube-TwophaseSolverFiveFaces-master
+C:\ProgramData\Anaconda3\python.exe "D:\Main\RubiksCubeSolver\NemaMachine\Code\brain\RubiksCube-TwophaseSolverFiveFaces-master\client_gui.py"
+cmd \k
---
+++ NemaMachine/Code/serverBatFiles/Server.bat
@@ -0,0 +1,7 @@
+title Cube Algorithm Solution Server
+
+
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+d:
+cd D:\Main\RubiksCubeSolver\NemaMachine\Code\brain\RubiksCube-TwophaseSolverFiveFaces-master
+C:\ProgramData\Anaconda3\python.exe "D:\Main\RubiksCubeSolver\NemaMachine\Code\brain\RubiksCube-TwophaseSolverFiveFaces-master\start_server.py"
