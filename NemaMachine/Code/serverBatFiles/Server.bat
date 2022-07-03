9118c83b73ab82ad5599d5608b567cd94f87781d
K R E A M <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2022-07-03 01:31:39 +0100

    updated

--- NemaMachine/Code/serverBatFiles/Server.bat
+++ NemaMachine/Code/serverBatFiles/Server.bat
@@ -5,3 +5,4 @@ call C:\ProgramData\Anaconda3\Scripts\activate.bat
 d:
 cd D:\Main\RubiksCubeSolver\NemaMachine\Code\brain\RubiksCube-TwophaseSolverFiveFaces-master
 C:\ProgramData\Anaconda3\python.exe "D:\Main\RubiksCubeSolver\NemaMachine\Code\brain\RubiksCube-TwophaseSolverFiveFaces-master\start_server.py"
+cmd \k
